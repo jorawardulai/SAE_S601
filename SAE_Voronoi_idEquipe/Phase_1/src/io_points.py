a277bcfbc386df4b8221fd02c06caaf3bbf050cd
brunel-benoit <benoit.brunel@edu.univ-paris13.fr>
2026-02-21 16:46:05 +0100

    ajout de json

--- SAE_Voronoi_idEquipe/Phase_1/src/io_points.py
+++ SAE_Voronoi_idEquipe/Phase_1/src/io_points.py
@@ -4,10 +4,20 @@ def importFichier(filepath) :
 
     coordonnee = []
 
-    with open(filepath, 'r') as fichier :
-        for ligne in fichier :
-            if ligne.strip():
-                x,y = ligne.strip().split(",")
-                coordonnee.append([float(x), float(y)])
+    if filepath.endswith(".txt") :
+
+        with open(filepath, 'r') as fichier :
+            for ligne in fichier :
+                if ligne.strip():
+                    x,y = ligne.strip().split(",")
+                    coordonnee.append([float(x), float(y)])
+
+    elif filepath.endswith(".json") :
+        with open(filepath, "r") as fichier :
+            data = json.load(fichier):
+
+            for p in data : 
+                coordonnee.append([float(p[0]), float(p[1])])
+
     
     return coordonnee
