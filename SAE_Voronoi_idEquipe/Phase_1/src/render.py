55ef4343df9eb30cec407e35220ec679359ca4de
Rodrigues-Kevin <rodrigues.kevin.pro123@gmail.com>
2026-02-21 19:25:20 +0100

    Encapsulation de la conversion en SVG dans une fonction

--- SAE_Voronoi_idEquipe/Phase_1/src/render.py
+++ SAE_Voronoi_idEquipe/Phase_1/src/render.py
@@ -4,7 +4,9 @@ from PIL import Image
 diagram = {}
 
 #Conversion du diagramme de Voronoï en image SVG
-svg_diagram = Image.fromarray(diagram).convert("RGB")
+def diagram_to_svg(diagram: dict) -> Image:
+    svg_diagram = Image.fromarray(diagram).convert("RGB")
+    return svg_diagram
 
 #Exportation de l'image du diagramme de Voronoï à un format voulu
 def export_diagram(svg_diagram: dict, format: str):
