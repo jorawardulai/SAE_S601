34c82844ceda413a261ba313fa6a2a28a51668c1
Rodrigues-Kevin <rodrigues.kevin.pro123@gmail.com>
2026-02-21 19:17:42 +0100

    Conversion du diagramme de Voronoï en image SVG

---
+++ SAE_Voronoi_idEquipe/Phase_1/src/render.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+#Résultat du diagramme de Voronoï
+diagram = {}
+
+#Conversion du diagramme de Voronoï en image SVG
+svg_diagram = Image.fromarray(diagram).convert("RGB")
+
+#Exportation de l'image du diagramme de Voronoï à un format voulu
+def export_diagram(svg_diagram: dict, format: str):
+    if format == "svg":
+        pass
+    elif format == "jpg":
+        pass
+    elif format == "png":
+        pass
+    else:
+        raise ValueError("Format non supporté.")
+    
