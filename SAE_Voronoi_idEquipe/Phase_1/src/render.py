ce27b719fc193bad5ccb24fd400bf2d56272b39c
Rodrigues-Kevin <rodrigues.kevin.pro123@gmail.com>
2026-02-21 21:44:33 +0100

    Calcul des dimensions du PNG selon la coordonnée absolue la plus grande du diagramme de Voronoï

--- SAE_Voronoi_idEquipe/Phase_1/src/render.py
+++ SAE_Voronoi_idEquipe/Phase_1/src/render.py
@@ -1,5 +1,6 @@
-from PIL import Image
 from voronoi import calculer_diagramme
+from PIL import Image
+import math
 
 #Résultat du diagramme de Voronoï
 def get_diagram():
@@ -8,8 +9,21 @@ def get_diagram():
 
 #Conversion du diagramme de Voronoï en image PNG
 def diagram_to_png(diagram: dict) -> Image:
-    png_diagram = Image.fromarray(diagram).convert("RGB")
-    return png_diagram
+    vertices = diagram["sommet"]
+    edges = diagram["aretes"]
+
+    abs_edges = []
+    for edge in edges:
+        for point in edge:
+            abs_edges.append((abs(point[0]), abs(point[1])))
+
+    image_size = math.ceil(max(max(abs_edges)))
+    image = Image.new("RGB", (image_size, image_size), "white")
+    image.show()
+
+    print(image_size)
+    #png_diagram = Image.fromarray(diagram).convert("RGB")
+    #return png_diagram
 
 #Exportation de l'image du diagramme de Voronoï à un format voulu
 def export_diagram(png_diagram: Image, format: str):
@@ -24,5 +38,6 @@ def export_diagram(png_diagram: Image, format: str):
 
 diagram = get_diagram()
 print(diagram)
+diagram_to_png(diagram)
 #image = Image.open("SAE_Voronoi_idEquipe\Phase_1\src\image.png")
 #export_diagram(image, "jpg")
