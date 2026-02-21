6469f9426e0c1cd6dc9921ddc842ba75cb97dd63
Rodrigues-Kevin <rodrigues.kevin.pro123@gmail.com>
2026-02-21 20:03:18 +0100

    Changement SVG en PNG

--- SAE_Voronoi_idEquipe/Phase_1/src/render.py
+++ SAE_Voronoi_idEquipe/Phase_1/src/render.py
@@ -3,18 +3,18 @@ from PIL import Image
 #Résultat du diagramme de Voronoï
 diagram = {}
 
-#Conversion du diagramme de Voronoï en image SVG
-def diagram_to_svg(diagram: dict) -> Image:
-    svg_diagram = Image.fromarray(diagram).convert("RGB")
-    return svg_diagram
+#Conversion du diagramme de Voronoï en image PNG
+def diagram_to_png(diagram: dict) -> Image:
+    png_diagram = Image.fromarray(diagram).convert("RGB")
+    return png_diagram
 
 #Exportation de l'image du diagramme de Voronoï à un format voulu
-def export_diagram(svg_diagram: Image, format: str):
-    if format == "svg":
+def export_diagram(png_diagram: Image, format: str):
+    if format == "png":
         pass
     elif format == "jpg":
         pass
-    elif format == "png":
+    elif format == "svg":
         pass
     else:
         raise ValueError("Format non supporté.")
